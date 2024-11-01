3b062b9f8b638a082fed808e6cde09acc2b43942
Ralph Liu <137829296+nv-rliu@users.noreply.github.com>
2024-11-01 15:46:00 -0400

    [CI] Adding CI Workflows: checks, changed-files, builds (#6)
    Closes https://github.com/rapidsai/graph_dl/issues/636 
    Closes https://github.com/rapidsai/graph_dl/issues/637
    
    This PR adds the following jobs in CI:
     - `changed-files`
     - `conda-python-build`
     - `wheel-build-nx-cugraph`
    
    Authors:
      - Ralph Liu (https://github.com/nv-rliu)
      - Erik Welch (https://github.com/eriknw)
      - Divye Gala (https://github.com/divyegala)
      - Kyle Edwards (https://github.com/KyleFromNVIDIA)
      - James Lamb (https://github.com/jameslamb)
      - Bradley Dice (https://github.com/bdice)
      - Rick Ratzel (https://github.com/rlratzel)
      - Vyas Ramasubramani (https://github.com/vyasr)
      - GALI PREM SAGAR (https://github.com/galipremsagar)
      - Tingyu Wang (https://github.com/tingyu66)
      - Alex Barghi (https://github.com/alexbarghi-nv)
    
    Approvers:
      - James Lamb (https://github.com/jameslamb)
    
    URL: https://github.com/rapidsai/nx-cugraph/pull/6

---
+++ ci/build_python.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright (c) 2022-2024, NVIDIA CORPORATION.
+
+set -euo pipefail
+
+rapids-configure-conda-channels
+
+source rapids-date-string
+
+rapids-print-env
+
+rapids-generate-version > ./VERSION
+rapids-logger "Begin py build"
+
+# TODO: Remove `--no-test` flags once importing on a CPU
+# node works correctly
+
+# NOTE: nothing in nx-cugraph is CUDA-specific, but it is built on each CUDA
+# platform to ensure it is included in each set of artifacts, since test
+# scripts only install from one set of artifacts based on the CUDA version used
+# for the test run.
+RAPIDS_PACKAGE_VERSION=$(head -1 ./VERSION) rapids-conda-retry mambabuild \
+  --no-test \
+  conda/recipes/nx-cugraph
+
+rapids-upload-conda-to-s3 python
---
+++ ci/build_wheel.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright (c) 2023-2024, NVIDIA CORPORATION.
+
+set -euo pipefail
+
+package_name=$1
+package_dir=$2
+
+source rapids-date-string
+
+RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
+
+rapids-generate-version > ./VERSION
+
+cd "${package_dir}"
+
+python -m pip wheel \
+    -w dist \
+    -v \
+    --no-deps \
+    --disable-pip-version-check \
+    --extra-index-url https://pypi.nvidia.com \
+    .
+
+RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" RAPIDS_PY_WHEEL_PURE="1" rapids-upload-wheels-to-s3 python dist
