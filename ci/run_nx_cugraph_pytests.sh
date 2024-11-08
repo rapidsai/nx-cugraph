ea00a10397d27c677d0f8967b0ea2ab9337bacc3
Erik Welch <erik.n.welch@gmail.com>
2024-11-08 10:46:31 -0600

    run_nx_cugraph_pytests.sh verbose for first pytest

--- ci/run_nx_cugraph_pytests.sh
+++ ci/run_nx_cugraph_pytests.sh
@@ -6,5 +6,6 @@ set -euo pipefail
 # Support invoking run_nx_cugraph_pytests.sh outside the script directory
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/..
 
-NX_CUGRAPH_USE_COMPAT_GRAPHS=False pytest --capture=no --cache-clear --benchmark-disable "$@" ./nx_cugraph/tests
-NX_CUGRAPH_USE_COMPAT_GRAPHS=True pytest --capture=no --cache-clear --benchmark-disable "$@" ./nx_cugraph/tests
+# Only be verbose and display print information for the first pytest command here
+NX_CUGRAPH_USE_COMPAT_GRAPHS=False pytest --capture=no --verbose --cache-clear --benchmark-disable "$@" ./nx_cugraph/tests
+NX_CUGRAPH_USE_COMPAT_GRAPHS=True pytest --capture=yes --cache-clear --benchmark-disable "$@" ./nx_cugraph/tests
