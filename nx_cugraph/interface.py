ff75d3d374bd65c67bcf0fe664d8b683d1eac4e5
Erik Welch <erik.n.welch@gmail.com>
2024-11-08 10:00:59 -0600

    Fix `_nxver` comparisons for variable length tuples

--- nx_cugraph/interface.py
+++ nx_cugraph/interface.py
@@ -228,7 +228,7 @@ class BackendInterface:
                 }
             )
 
-        if _nxver <= (3, 2):
+        if _nxver < (3, 3):
             xfail.update(
                 {
                     # NetworkX versions prior to 3.2.1 have tests written to
@@ -265,7 +265,7 @@ class BackendInterface:
                 }
             )
 
-        if _nxver <= (3, 1):
+        if _nxver < (3, 2):
             # MAINT: networkx 3.0, 3.1
             # NetworkX 3.2 added the ability to "fallback to nx" if backend algorithms
             # raise NotImplementedError or `can_run` returns False. The tests below
