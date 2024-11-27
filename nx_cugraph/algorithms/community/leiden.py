31dc9a328e580eec8dceb96a6c477c62516dee3b
Erik Welch <erik.n.welch@gmail.com>
2024-11-26 19:38:37 -0600

    Remove unused `theta` parameter; minor updates

--- nx_cugraph/algorithms/community/leiden.py
+++ nx_cugraph/algorithms/community/leiden.py
@@ -16,9 +16,11 @@ import pylibcugraph as plc
 
 from nx_cugraph.convert import _to_undirected_graph
 from nx_cugraph.utils import (
+    _dtype_param,
     _get_float_dtype,
     _groupby,
     _seed_to_int,
+    networkx_algorithm,
     not_implemented_for,
 )
 
@@ -26,18 +28,12 @@ __all__ = ["leiden_communities"]
 
 
 @not_implemented_for("directed")
-# @networkx_algorithm(extra_params=_dtype_param, version_added="25.02", _plc="leiden")
+@networkx_algorithm(extra_params=_dtype_param, version_added="25.02", _plc="leiden")
 def leiden_communities(
-    G, weight="weight", resolution=1, theta=1, max_level=None, seed=None, *, dtype=None
+    G, weight="weight", resolution=1, max_level=None, seed=None, *, dtype=None
 ):
-    """Find the best partition of a graph using Leiden Community Detection algorithm.
-
-    **Warning: this API is experimental and may change. It is not yet in NetworkX.**
-
-    See Also
-    --------
-    louvain_communities
-    """
+    # Warning: this API is experimental and may change. It is not yet in NetworkX.
+    # See: https://github.com/networkx/networkx/pull/7743
     seed = _seed_to_int(seed)
     G = _to_undirected_graph(G, weight, 1, np.float32)
     dtype = _get_float_dtype(dtype, graph=G, weight=weight)
@@ -49,7 +45,7 @@ def leiden_communities(
         graph=G._get_plc_graph(weight, 1, dtype),
         max_level=max_level,
         resolution=resolution,
-        theta=theta,
+        theta=1,  # TODO: expose theta as a backend-only parameter once it's used
         do_expensive_check=False,
     )
     groups = _groupby(clusters, node_ids, groups_are_canonical=True)
--- nx_cugraph/tests/test_leiden.py
+++ nx_cugraph/tests/test_leiden.py
@@ -15,7 +15,8 @@ import nx_cugraph as nxcg
 
 
 def test_leiden_karate():
+    # Basic smoke test; if something here changes, we want to know!
     G = nxcg.karate_club_graph()
-    leiden = nxcg.community.leiden_communities(G)
-    louvain = nxcg.community.louvain_communities(G)
+    leiden = nxcg.community.leiden_communities(G, seed=123)
+    louvain = nxcg.community.louvain_communities(G, seed=123)
     assert leiden == louvain
