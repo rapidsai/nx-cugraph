0d18399a825f95b75d30c3d70ed01a241b6eaa70
Ralph Liu <ralphl@nvidia.com>
2025-03-19 08:51:48 -0700

    Initial test

--- nx_cugraph/tests/test_layout.py
+++ nx_cugraph/tests/test_layout.py
@@ -14,10 +14,20 @@
 import networkx as nx
 import pytest
 
+import nx_cugraph as nxcg
+
 # The tests in this file cover use cases unique to nx-cugraph.  If the coverage
 # here is not unique to nx-cugraph, consider moving those tests to the NetworkX
 # project.
 
 
-def test_forceatlas2():
-	...
+@pytest.mark.parametrize("get_graph", [nx.karate_club_graph, nx.les_miserables_graph])
+def test_forceatlas2(get_graph):
+    Gnx = get_graph()
+    Gcg = nxcg.from_networkx(Gnx, preserve_all_attrs=True)
+
+    res_nx = nx.forceatlas2_layout(Gnx)
+    res_cg = nxcg.forceatlas2_layout(Gcg)
+
+    assert len(res_nx) == len(res_cg)
+    # for each pos element in res_nx assert that it's equal to res_cg
