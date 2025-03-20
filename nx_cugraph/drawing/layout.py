8c4024f1a42fd6d6c8095f528b46dffa13abdaa9
Ralph Liu <ralphl@nvidia.com>
2025-03-20 09:16:38 -0700

    Ignore newlining suggestion by ruff

--- nx_cugraph/drawing/layout.py
+++ nx_cugraph/drawing/layout.py
@@ -11,6 +11,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# ruff: noqa: D205
+
 import cupy as cp
 import networkx as nx
 import pylibcugraph as plc
