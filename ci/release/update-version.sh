c9c3e92feb4b023f0142aa23cd566ecfa60e9963
Ray Douglass <ray@raydouglass.com>
2025-04-30 15:18:00 -0400

    Fix update_version.sh

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -48,7 +48,7 @@ DEPENDENCIES=(
   nx-cugraph
   pylibcugraph
 )
-for FILE in dependencies.yaml conda/environments/*.yaml conda/recipes/**/meta.yaml; do
+for FILE in dependencies.yaml conda/environments/*.yaml; do
   for DEP in "${DEPENDENCIES[@]}"; do
     sed_runner "/-.* ${DEP}\(-cu[[:digit:]]\{2\}\)\{0,1\}==/ s/==.*/==${NEXT_SHORT_TAG_PEP440}.*,>=0.0.0a0/g" "${FILE}"
   done
