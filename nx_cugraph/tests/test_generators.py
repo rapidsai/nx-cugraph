72b3efddba09b0433c62162cc21a8111ef651ef7
Ralph Liu <ralphl@nvidia.com>
2025-07-01 10:28:53 -0700

    Comment out WIP tests from other PR

--- nx_cugraph/tests/test_generators.py
+++ nx_cugraph/tests/test_generators.py
@@ -79,7 +79,7 @@ def compare(name, create_using, *args, is_vanilla=False):
 
 
 N = list(range(-1, 5))
-N_AND_NODES = N + ["a", "aa", "aba", "bba", "abba", "abc", "abcb", [0, 1, 2]]
+# N_AND_NODES = N + ["a", "aa", "aba", "bba", "abba", "abc", "abcb", [0, 1, 2]]
 CREATE_USING = [nx.Graph, nx.DiGraph, nx.MultiGraph, nx.MultiDiGraph]
 COMPLETE_CREATE_USING = [
     nx.Graph,
@@ -220,15 +220,17 @@ def test_generator_n_complete(name, n, create_using):
     compare(name, create_using, n)
 
 
-GENERATORS_N_AND_STRINGS = list(GENERATORS_N)
+# TODO: finish these tests started in
+# https://github.com/rapidsai/nx-cugraph/pull/143/
+# GENERATORS_N_AND_STRINGS = list(GENERATORS_N)
 
 
-@pytest.mark.parametrize("name", GENERATORS_N_AND_STRINGS)
-@pytest.mark.parametrize("n", N_AND_NODES)
-@pytest.mark.parametrize("create_using", CREATE_USING)
-def test_generator_n_and_nodes(name, n, create_using):
-    print(name, n, create_using)
-    compare(name, create_using, n)
+# @pytest.mark.parametrize("name", GENERATORS_N_AND_STRINGS)
+# @pytest.mark.parametrize("n", N_AND_NODES)
+# @pytest.mark.parametrize("create_using", CREATE_USING)
+# def test_generator_n_and_nodes(name, n, create_using):
+#     print(name, n, create_using)
+#     compare(name, create_using, n)
 
 
 @pytest.mark.parametrize("name", GENERATORS_M_N)
