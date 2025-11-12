1811541c15f8bfb6f0c33eb218bdc77e97d65b02
Gil Forsyth <gforsyth@users.noreply.github.com>
2025-11-12 10:42:42 -0500

    ci(build): switch to pure wheel and noarch: python builds (#198)
    This switches the wheel builds to run a single build per CUDA major version, and the mamba builds to be a signle `noarch: python` build.
    
    The uploaded build artifacts are downloaded by all the test runners (either wheels or conda packages) and the test matrix remains the same.
    
    Authors:
      - Gil Forsyth (https://github.com/gforsyth)
    
    Approvers:
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    URL: https://github.com/rapidsai/nx-cugraph/pull/198

--- ci/build_wheel_nx-cugraph.sh
+++ ci/build_wheel_nx-cugraph.sh
@@ -8,3 +8,6 @@ source rapids-init-pip
 
 ./ci/build_wheel.sh .
 ./ci/validate_wheel.sh "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
+
+RAPIDS_PACKAGE_NAME="$(rapids-package-name wheel_python nx-cugraph --pure --cuda "$RAPIDS_CUDA_VERSION")"
+export RAPIDS_PACKAGE_NAME
--- ci/test_wheel_nx-cugraph.sh
+++ ci/test_wheel_nx-cugraph.sh
@@ -9,10 +9,8 @@ source rapids-init-pip
 package_name="nx-cugraph"
 python_package_name=${package_name//-/_}
 
-RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
-
 # nx-cugraph is a pure wheel, which is part of generating the download path
-NX_CUGRAPH_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" RAPIDS_PY_WHEEL_PURE="1" rapids-download-wheels-from-github python)
+NX_CUGRAPH_WHEELHOUSE=$(rapids-download-from-github "$(rapids-package-name "wheel_python" "$package_name" --pure --cuda "$RAPIDS_CUDA_VERSION")")
 
 # echo to expand wildcard before adding `[extra]` requires for pip
 rapids-pip-retry install \
