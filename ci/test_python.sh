02f4fe01897886a45f135b5f26dd5ee965b4f224
Bradley Dice <bdice@bradleydice.com>
2025-11-21 00:43:18 -0600

    Use strict priority in CI conda tests

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -9,6 +9,9 @@ cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../
 
 . /opt/conda/etc/profile.d/conda.sh
 
+rapids-logger "Configuring conda strict channel priority"
+conda config --set channel_priority strict
+
 rapids-logger "Downloading artifacts from previous jobs"
 PYTHON_CHANNEL=$(rapids-download-from-github "$(rapids-package-name "conda_python" nx-cugraph --pure)")
 
