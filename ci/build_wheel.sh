80dd34a0917bc25a95767e1ac056676a0a2d7f91
Mike McCarty <mmccarty@nvidia.com>
2026-01-20 08:23:59 -0500

    wheel builds: react to changes in pip's handling of build constraints (#230)
    Contributes to https://github.com/rapidsai/build-planning/issues/242
    
    Modifying `ci/build_wheel.sh` to
    
    - pass`--build-constraint="${PIP_CONSTRAINT}"` unless build isolation is enabled.
    - unset `PIP_CONSTRAINT` (set by rapids-init-pip)... it doesn't affect builds as of pip 25.3, and results in an error from `pip wheel` when set and `--build-constraint` is also passed
    
    Authors:
      - Mike McCarty (https://github.com/mmccarty)
    
    Approvers:
      - James Lamb (https://github.com/jameslamb)
    
    URL: https://github.com/rapidsai/nx-cugraph/pull/230

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
 set -euo pipefail
@@ -13,10 +13,17 @@ rapids-generate-version > ./VERSION
 
 cd "${package_dir}"
 
+RAPIDS_PIP_WHEEL_ARGS=(
+  -w "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
+  -v
+  --no-deps
+  --disable-pip-version-check
+  --extra-index-url https://pypi.nvidia.com
+)
+
+# unset PIP_CONSTRAINT (set by rapids-init-pip)... it doesn't affect builds as of pip 25.3, and
+# results in an error from 'pip wheel' when set and --build-constraint is also passed
+unset PIP_CONSTRAINT
 rapids-pip-retry wheel \
-    -w "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}" \
-    -v \
-    --no-deps \
-    --disable-pip-version-check \
-    --extra-index-url https://pypi.nvidia.com \
+    "${RAPIDS_PIP_WHEEL_ARGS[@]}" \
     .
