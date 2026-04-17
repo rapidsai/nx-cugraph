a2a28d74a0194c082a10677e83a1f6af81e19103
Victor L <victor.lafargechx@gmail.com>
2026-04-17 17:55:49 +0200

    Added tests for forceatlas2 layout

--- nx_cugraph/tests/test_layout.py
+++ nx_cugraph/tests/test_layout.py
@@ -21,3 +21,44 @@ def test_forceatlas2(get_graph):
 
     assert len(res_nx) == len(res_cg)
     assert res_nx.keys() == res_cg.keys()
+
+
+def test_forceatlas2_extra_params():
+    Gnx = nx.karate_club_graph()
+    Gcg = nxcg.from_networkx(Gnx, preserve_all_attrs=True)
+
+    node_mobility = {n: 1.0 for n in Gcg}
+
+    res_cg = nxcg.forceatlas2_layout(
+        Gcg,
+        barnes_hut_optimize=True,
+        barnes_hut_theta=0.5,
+        node_mobility=node_mobility,
+    )
+
+    assert len(res_cg) == len(Gcg)
+    assert res_cg.keys() == nx.nodes(Gcg)
+
+
+
+def test_forceatlas2_max_iter_0():
+    G = nx.cycle_graph(5)
+    Gcg = nxcg.from_networkx(G, preserve_all_attrs=True)
+
+    initial_positions = {
+        0: (0.0, 1.0),
+        1: (2.0, 3.0),
+        2: (4.0, 5.0),
+        3: (6.0, 7.0),
+        4: (8.0, 9.0)
+    }
+
+    final_positions = nxcg.forceatlas2_layout(
+        Gcg,
+        pos=initial_positions,
+        max_iter=0,
+    )
+
+    for node, pos in initial_positions.items():
+        assert final_positions[node][0] == pos[0]
+        assert final_positions[node][1] == pos[1]
